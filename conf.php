5951067e51664dc79cec2835f92eddaa379b2191
Matt <96447310+mattqze@users.noreply.github.com>
2023-08-07 23:01:31 -0400

    Delete conf.php

--- conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Blog Setup
-$blogTitle = 'Tunnull\'s Blog';
-$blogDescription = 'Write an awesome description for your new site here. You can edit this line in conf.php. It will appear in your document head meta (for Google search results) and in your feed.xml site description.';
-$authorName = 'Tunnull';
-$authorEmail = 'ttam@tunnull.xyz';
-?>
